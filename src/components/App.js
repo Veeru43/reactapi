51d79c4cd13b75f1e355cec5548648de616cc24b
Veeru43 <veerababu.veeru1999@gmail.com>
2020-05-13 23:13:15 +0530

    react

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+import ImageList from './imageList';
+
+class App extends React.Component  {
+    state = { images: [] };
+
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID EGvdgv7v5sAAeFRk3Ca_mqKoOtxFh-E_oo1hJVH0DEI'
+            }
+        })
+
+        this.setState({ images: response.data.results })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar userSubmit={this.onSearchSubmit}/>
+                <span>Found: {this.state.images.length} images</span>
+                <ImageList foundImages={this.state.images} />
+            </div>
+        )
+    }
+
+}
+
+export default App;
+
